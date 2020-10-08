72029f49d644f213c1f07a6f6747ab39470754bc
Joz-Bruer Quince <quincejozbruer@gmail.com>
2020-10-07 19:36:09 -0700

    Add the toogle button, and switch from map to reviews"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false)
+  const [review, setReview] = useState(false)
+
+  useEffect(()=>{
+    // window.addEventListener("resize", () => {
+      const ismobile = window.innerWidth <= 425;
+      if (ismobile) setIsMobile(!isMobile);
+  // }, true);
+  }, [])
+
+  function handleSwich(){
+    setReview(!review)
+  }
   return (
     <>
       <Router>
@@ -12,11 +25,18 @@ function App() {
           <Route path='/' />
         </Switch>
       </Router>
+      <div className={`${isMobile ? "" : "displayButton"}`}>
+        <label onChange={handleSwich} className="switch">
+        <input type="checkbox" />
+        <span className="slider round"></span>
+      </label>
+       </div>
+      
       <div className='wrapper'>
-        <div className='map'>
+        <div className={`map ${review ? 'display' : ''}`}>
         <h1>Map Box</h1>
         </div>
-        <div className='reviews'>
+        <div className={`reviews ${review ? '' : 'display'}`}>
             <h1>Reviews Box</h1>
         </div>
       </div>
