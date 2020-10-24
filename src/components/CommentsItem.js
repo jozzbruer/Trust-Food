94e792d1338e6edc70f934c6297a843ccdca8c33
Joz-Bruer Quince <quincejozbruer@gmail.com>
2020-10-23 18:02:35 -0700

    Showing the stars in the comments

--- src/components/CommentsItem.js
+++ src/components/CommentsItem.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { ImStarFull } from 'react-icons/im'
 
 function CommentsItem(props) {
+    
+   function showStars(x){
+       let arr = []
+       let i = 0
+       while (i < x){
+           arr.push(<ImStarFull />)
+           i++
+       }
+       return arr;
+   }
     return (
         <div> 
-        <h4> {props.comment} </h4><span> {props.rate} stars</span>
+        <h4> {props.comment} </h4><span> {showStars(props.rate)}</span>
         </div>
     )
 }
